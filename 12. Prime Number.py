8589d0fc25a8f92ca43a852fc3586e7389820a39
Aayush-0409 <aayushkmehta04@gmail.com>
2026-07-03 20:21:19 +0530

    Initial Commit

---
+++ 12. Prime Number.py
@@ -0,0 +1,12 @@
+n = int(input("ENter a number :"))
+
+count = 0
+
+for i in range(1, n+1):
+    if n % i == 0:
+        count += 1
+
+if count == 2 :
+    print("Prime")
+else:
+    print("Not Prime")
---
+++ 7. Full Pyramid.py
@@ -0,0 +1,22 @@
+# n = 5 
+
+# for i in range(n):
+#     print(" "*(n-i-1), end="")
+#     print("*"*(2*i+1))
+
+#OR
+
+n = 5
+
+for i in range(1, n + 1):
+
+    # Print spaces
+    for j in range(n - i):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2 * i - 1):
+        print("*", end="")
+
+    print()
+
